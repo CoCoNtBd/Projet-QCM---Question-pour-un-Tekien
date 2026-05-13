a443f21fe2b70fb70071facb7c22a15a3e30e4c4
Coco <corentin.bernard1706@gmail.com>
2026-05-13 16:04:33 +0200

    Add functions to save QCM and questions
    Implement functions to save QCM and questions to a file.

--- qcm.c
+++ qcm.c
@@ -1,1 +1,25 @@
 
+void sauvegarder_QCM(QCM* qcm){
+    char nomFichier[100];
+    FILE* f = fopen(nomFichier, "w");
+    if(f == NULL){
+        printf("Erreur d'ouverture fichier");
+        return;
+    }
+    fprintf(f,"%s\n", qcm->nom);
+    fprintf(f,"%s\n", qcm->categorie);
+    fprintf(f,"%d;%d;%d\n", q.negatif, q.plsreponses, q.sequentiel);
+    fprintf(f,"%d\n", q.num_questions);
+    for(int j = 0; j<q.num_questions; j++){
+        
+    }
+}
+
+
+void sauvegarder_question(Question* q){
+    fprintf(f,"%s\n", q->texte);
+    fprintf(f,"%d\n", q->num_correct);
+    for(int k = 0; k<q->num_correct; k++){
+        fprintf(f,"%c:%s", 'A'+k, q->correct[i]);
+    }
+}
